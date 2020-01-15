402946f8a9c38fd9f9deb1429589358b91ad382b
loukass23 <l.dupias@net.estia.fr>
2020-01-15 17:12:46 +0100

    initial

---
+++ Assets/Ressources/Scripts/WeedBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeedBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int value;
+    public bool high;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
